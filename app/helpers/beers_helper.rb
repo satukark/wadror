e2150001f8c9b81d661b20151fef1f2ec54fab2b
satukark <satu81@hotmail.com>
2014-01-17 14:56:24 +0100

    update

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -1,5 +1,3 @@
 module BeersHelper
-      def pluralize(count, singular, plural = nil)
-        "#{count || 0} " + ((count == 1 || count == '1') ? singular : (plural || singular.pluralize))
-      end
+
 end
