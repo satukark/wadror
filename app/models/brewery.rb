6050bc49cb3c7cd4f82c0fc0a5ae98145fc9175f
Satu Kärkkäinen <iltasatu@gmail.com>
2014-01-29 23:21:50 +0100

    Validates korjattu

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,7 @@ class Brewery < ActiveRecord::Base
 	has_many :ratings, :through => :beers
 	
     validates_presence_of :name
-    validates_numericality_of :score, { greater_than_or_equal_to: 1042,
+    validates_numericality_of :year, { greater_than_or_equal_to: 1042,
                                       less_than_or_equal_to: 2014,
                                       only_integer: true }
   
